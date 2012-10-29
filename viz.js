6e9a0aa3ec9d4f84a1694b54359e4a091fa36032
Kenn Knowles <kenn.knowles@gmail.com>
2012-10-29 18:05:26 -0400

    Get clicks working on the accumulated bar chart, sort of

--- viz.js
+++ viz.js
@@ -1,8 +1,4 @@
    
-/* Global page state defaults */ 
-current_station_id = 38 // FIX ME: Hard coded what station to look at for now!
-current_hour_selected = 17 // FIX ME: This should be based on user input too. How to handle aggregate?
-
 /* Colors found in sass/viz.scss but if there's a clever way to automate extraction */
 var positive_color = '#36ac9c';
 var negative_color = '#f9a72b';
@@ -75,11 +71,14 @@ function bind_station_accumulation_data(svg, data) {
 	    .attr("width", x.rangeBand())
 	    .attr("height", height);
 
-    $('#aggregates').on("mouseover mouseout", ".activator", function(event) {
+    /* In order to always catch events from dynamically generated content, the parent div is where we bind */
+    $('#aggregates').on("mouseover mouseout click", "rect", function(event) {
         if (event.type == "mouseover") {
             $(this).attr("class", "activator active");
         } else if (event.type == "mouseout") {
             $(this).attr("class", "activator");
+        } else if (event.type == "click") {
+            current_station_id( $(this).attr("data-station") );
         }
     });
 }
@@ -142,6 +141,78 @@ function bind_map_data(map, data) {
     // TODO: bind on click to mutate selected station id
 }
 
+function set_up_station_chart() {
+    // Begin Graphical Elements for Station Chart
+    // sc for station_chart
+    // Margin convention from here: http://bl.ocks.org/3019563
+    var width = $('#line-chart').width();    // TODO how to make width '100%' again dynamically?, use width of parent?
+    var height = $('#line-chart').height();
+    var margin_bottom = 30; // This has to be within the SVG, to make room for x axis labels, but nothing else does
+
+    var chart_svg = d3.select('#line-chart').append('svg')
+        //.attr('style', 'border: 1px solid red') // For debugging
+        .attr('width', width)
+        .attr('height', height);
+
+    return chart_svg;
+}
+    
+function bind_station_chart_data(chart_svg, one_station_departures, one_station_arrivals) {
+    var width = $('#line-chart').width();    // TODO how to make width '100%' again dynamically?, use width of parent?
+    var height = $('#line-chart').height();
+    var margin_bottom = 30; // This has to be within the SVG, to make room for x axis labels, but nothing else does
+
+    var one_station_max = Math.max(_.max(one_station_departures),
+				                   _.max(one_station_arrivals))
+    
+    var x_scale = d3.scale.ordinal()
+        .domain(_.range(0, 23))
+        .rangeRoundBands([0, width]);
+    
+    var y_scale = d3.scale.linear() 
+        .domain([0, one_station_max])
+        .range([height-margin_bottom, 0]);
+    
+    var line = d3.svg.line()
+        .x(function(d, i) { return x_scale(i); })
+        .y(function(d) { return y_scale(d); });
+    
+    chart_svg.append("path")
+        .datum(one_station_arrivals)
+        .attr("class", "line arrivals")
+        .attr("d", line);
+    chart_svg.append("path")
+        .datum(one_station_departures)
+        .attr("class", "line departures")
+        .attr("d", line);
+    
+    sc_x_axis = d3.svg.axis()
+	    .scale(x_scale)
+	    .orient("bottom")
+	//.ticks(d3.time.hours,2);
+	    .tickValues([0,4,8,12,16, 20]); // TODO, should wrap data so that you can see continuity over midnight - 2am?
+    
+    sc_y_axis = d3.svg.axis()
+	    .scale(y_scale)
+	    .orient("right")
+	    .ticks(5);
+    
+    chart_svg.append("g")
+	    .attr("class", "axis")
+	    .attr("transform", "translate(0,"+ (height - margin_bottom) + ")")
+	    .call(sc_x_axis);
+    
+    chart_svg.append("g")
+	    .attr("class", "y axis")
+	    .call(sc_y_axis)
+	    .append("text")
+	    .attr("transform", "rotate(-90)")
+        .attr("y", 30) // Does this make sense?
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Average Weekday Trips");
+}
+
 $(document).ready(function() {
 
     $("#slider").slider({
@@ -151,14 +222,18 @@ $(document).ready(function() {
         step: 1,
         change: function(event, ui){
             console.log(ui.value);
-            current_station_id = ui.value;
+            current_station_id(ui.value);
         }
     });
 
     /* Page View State Variables */
-    current_station_id = $.url().param('station') || current_station_id;
-    current_hour_selected = $.url().param('hour') || current_hour_selected; 
-    console.log('Current station / hour:', current_station_id, current_hour_selected);
+    current_station_id = ko.observable();
+    current_hour_selected = ko.observable();
+
+    var dummy = ko.computed(function() {
+        // Just monitors the values
+        console.log('Current station / hour:', current_station_id(), current_hour_selected());
+    });
     
     /* Massage the initial data to make life a little easier */
     stations = _(stations).filter(function(station) { return !station.temporary });
@@ -173,91 +248,72 @@ $(document).ready(function() {
         d.station = stations_by_id[d.station_id];
     });
 
-    /* Initial data */
-    var current_hour_data = accumulation_data_for_hour(current_hour_selected);
+    /* Derived data */
+    var current_hour_data = ko.computed(function() {
+        var result = accumulation_data_for_hour(current_hour_selected);
+        console.log('Accumulation data:', result);
+        return result;
+    });
     
-    var one_station_data_arrivals = _.chain(hourly_data)
-	.filter(function(d) { return d.station_id == current_station_id;})
-	.sortBy(function(d) { return d.hour })
-	.map(function(d) { return d.arrivals;})
-	.value();
+    var one_station_data_arrivals = ko.computed(function() {
+        var station_id = current_station_id();
 
-    var one_station_data_departures = _.chain(hourly_data)
-	.filter(function(d) { return d.station_id == current_station_id;})
-	.sortBy(function(d) { return d.hour })
-	.map(function(d) { return d.departures;})
-	.value();
+        if(!station_id) {
+            return null
+        } else {
+            var result = _.chain(hourly_data)
+                .filter(function(d) { return d.station_id == station_id;})
+                .sortBy(function(d) { return d.hour })
+                .map(function(d) { return d.arrivals;})
+                .value();
+            console.log('Station departures:', result);
+            return result;
+        }
+    });
 
-    var one_station_max = Math.max(_.max(one_station_data_departures),
-				   _.max(one_station_data_arrivals))
+    var one_station_data_departures = ko.computed(function() {
+        var station_id = current_station_id();
 
-    /* Set up the station accumulation chart with some initial data */
+        if (!station_id) {
+            return null;
+        } else {
+            var result = _.chain(hourly_data)
+                .filter(function(d) { return d.station_id == station_id;})
+                .sortBy(function(d) { return d.hour })
+                .map(function(d) { return d.departures;})
+                .value();
+            console.log('Station departures:', result);
+            return result;
+        }
+    });
+
+    /* Set up the station accumulation chart and subscribe to data changes */
     var accumulations_svg = set_up_station_accumulations();
-    bind_station_accumulation_data(accumulations_svg, current_hour_data); // FIXME: the default should be a sum, not the current hour
+    var dummy = ko.computed(function() { bind_station_accumulation_data(accumulations_svg, current_hour_data()); })
     
-    /* Set up the Map with initial data */
+    /* Set up the Map and subscribe to data changes */
     var map = set_up_map(stations);
-    bind_map_data(map, current_hour_data);
+    var dummy = ko.computed(function() { bind_map_data(map, current_hour_data()); });
     
-    // Begin Graphical Elements for Station Chart
-    // sc for station_chart
-    // Margin convention from here: http://bl.ocks.org/3019563
-    var width = $('#line-chart').width();    // TODO how to make width '100%' again dynamically?, use width of parent?
-    var height = $('#line-chart').height();
-    var margin_bottom = 30; // This has to be within the SVG, to make room for x axis labels, but nothing else does
+    /* Set up the station chart and subscribe to data changes */
+    var station_chart_svg = null; // This will start blank and be added when data is ready
+    var dummy = ko.computed(function() { 
+        var arrivals = one_station_data_arrivals();
+        var departures = one_station_data_departures();
+        console.log('Rebinding');
 
-    var chart_svg = d3.select('#line-chart').append('svg')
-        //.attr('style', 'border: 1px solid red') // For debugging
-        .attr('width', width)
-        .attr('height', height);
-    
+        if (arrivals && departures) {
+            $('#line-chart').text();
+            if (!station_chart_svg) {
+                station_chart_svg = set_up_station_chart();
+            }
+            bind_station_chart_data(station_chart_svg, arrivals, departures);
+        } else {
+            $('#line-chart').text('Choose a station to see details.');
+        }
+    });
 
-    chart_svg.selectAll();
-
-    var x_scale = d3.scale.ordinal()
-        .domain(_.range(0, 23))
-        .rangeRoundBands([0, width]);
-
-    var y_scale = d3.scale.linear() 
-        .domain([0, one_station_max])
-        .range([height-margin_bottom, 0]);
-
-    var line = d3.svg.line()
-        .x(function(d, i) { return x_scale(i); })
-        .y(function(d) { return y_scale(d); });
-
-    chart_svg.append("path")
-        .datum(one_station_data_arrivals)
-        .attr("class", "line arrivals")
-        .attr("d", line);
-    chart_svg.append("path")
-        .datum(one_station_data_departures)
-        .attr("class", "line departures")
-        .attr("d", line);
-
-    sc_x_axis = d3.svg.axis()
-	.scale(x_scale)
-	.orient("bottom")
-	//.ticks(d3.time.hours,2);
-	.tickValues([0,4,8,12,16, 20]); // TODO, should wrap data so that you can see continuity over midnight - 2am?
-
-    sc_y_axis = d3.svg.axis()
-	.scale(y_scale)
-	.orient("right")
-	.ticks(5);
-
-    chart_svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(0,"+ (height - margin_bottom) + ")")
-	.call(sc_x_axis);
-
-    chart_svg.append("g")
-	.attr("class", "y axis")
-	.call(sc_y_axis)
-	.append("text")
-	.attr("transform", "rotate(-90)")
-          .attr("y", 30) // Does this make sense?
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Average Weekday Trips");
+    /* Now that everything is ready, load from querystring */
+    current_station_id($.url().param('station'));
+    current_hour_selected($.url().param('hour'));
 });
