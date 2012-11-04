e85a5ac85ab4abfd0de3e5537be434c1876c718d
Zia Sobhani <zsobhani@gmail.com>
2012-11-03 20:31:28 -0400

    corrected highlight color from debugging

--- viz.js
+++ viz.js
@@ -7,7 +7,7 @@ var neutral_color = '#a1988d';'#9b8978';// #6f5d4d';//'#c9baad';//'#b4aca8';//c7
 
 //With 25% opacity the mix is this: "#d1d3b1";//"#ac9faa"
 var acc_y_ax_pad = 50;
-var highlighted_color = '#ff0000';
+var highlighted_color = '#ffffff';
 var selected_color = '#fddf24';
 var excessFactor = 0.2; // corredonds to Zia's old value of 1.5, with new math arrive/depart > (1+F)/(1-F).
 // arrive = 1.5*depart >> arrive = 60% of total traffic, which seems like the easiest way to talk abot it. .2 means 20% of middle is considered neutral...
